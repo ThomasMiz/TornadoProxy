de4b6fd980ee0d92e3e8bd3ff7099a8142ea0c1a
camilaDiToro <camiladitoro@gmail.com>
2022-11-01 13:31:17 -0300

    Add a macro to calculate the length of an array

--- test/test_negotiation.c
+++ test/test_negotiation.c
@@ -16,7 +16,7 @@ END_TEST
 START_TEST(complete_request_invalid_version) {
     negParser* p = newNegotiationParser();
     uint8_t data[] = {0x03, 0x01, 0x00};
-    negState state = negotiationRead(p, data, 3);
+    negState state = negotiationRead(p, data, length(data));
     fail_unless(state == NEG_ERROR);
     fail_unless(p->authMethod == NEG_METHOD_NO_MATCH);
     freeNegotiationParser(p);
@@ -26,7 +26,7 @@ END_TEST
 START_TEST(complete_request_invalid_versions) {
     negParser* p = newNegotiationParser();
     uint8_t data[] = {0x03, 0x02, 0x08, 0x05};
-    negState state = negotiationRead(p, data, 4);
+    negState state = negotiationRead(p, data, length(data));
     fail_unless(state == NEG_ERROR);
     fail_unless(p->authMethod == NEG_METHOD_NO_MATCH);
     freeNegotiationParser(p);
@@ -36,7 +36,7 @@ END_TEST
 START_TEST(complete_request_no_match_x2) {
     negParser* p = newNegotiationParser();
     uint8_t data[] = {0x05, 0x02, 0x08, 0x05};
-    negState state = negotiationRead(p, data, 4);
+    negState state = negotiationRead(p, data, length(data));
     fail_unless(state == NEG_END);
     fail_unless(p->authMethod == NEG_METHOD_NO_MATCH);
     freeNegotiationParser(p);
@@ -46,7 +46,7 @@ END_TEST
 START_TEST(complete_request_match_no_auth) {
     negParser* p = newNegotiationParser();
     uint8_t data[] = {0x05, 0x02, 0x08, 0x00};
-    negState state = negotiationRead(p, data, 4);
+    negState state = negotiationRead(p, data, length(data));
     fail_unless(state == NEG_END);
     fail_unless(p->authMethod == NEG_METHOD_NO_AUTH);
     freeNegotiationParser(p);
