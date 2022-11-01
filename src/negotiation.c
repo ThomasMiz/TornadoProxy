9606b8107976b872ffc4d19def71888b40df9e27
camilaDiToro <camiladitoro@gmail.com>
2022-11-01 12:55:20 -0300

    Unify constants names

--- src/negotiation.c
+++ src/negotiation.c
@@ -8,28 +8,25 @@ negState parseVersion(negParser* p, uint8_t c);
 negState parseMethodCount(negParser* p, uint8_t c);
 negState parseMethods(negParser* p, uint8_t c);
 negState parseEnd(negParser* p, uint8_t c);
-negState parseError(negParser* p, uint8_t c);
 
 static parseCharacter stateRead[] = {
     /* VERSION      */ (parseCharacter)parseVersion,
     /* METHOD_COUNT */ (parseCharacter)parseMethodCount,
     /* METHODS      */ (parseCharacter)parseMethods,
     /* END          */ (parseCharacter)parseEnd,
-    /* ERROR        */ (parseCharacter)parseError,
-};
+    /* ERROR        */ (parseCharacter)parseEnd};
 
 negState negotiationRead(negParser* p, uint8_t* buffer, int bufferSize) {
-    negState state = p->state;
-    for (int i = 0; i < bufferSize && state != ERROR && state != END; i++) {
-        state = stateRead[state](p, buffer[i]);
+    for (int i = 0; i < bufferSize && p->state != NEG_ERROR && p->state != NEG_END; i++) {
+        p->state = stateRead[p->state](p, buffer[i]);
     }
-    return p->state = state;
+    return p->state;
 }
 
 negParser* newNegotiationParser() {
     negParser* p = calloc(sizeof(negParser), 1);
-    p->state = VERSION;
-    p->authMethod = NEGOTIATION_METHOD_NO_MATCH;
+    p->state = NEG_VERSION;
+    p->authMethod = NEG_METHOD_NO_MATCH;
     return p;
 }
 
@@ -40,40 +37,34 @@ void freeNegotiationParser(negParser* p) {
 negState parseVersion(negParser* p, uint8_t c) {
     if (c != 5) {
         printf("[ERR] Client specified invalid version: %d\n", c);
-        return ERROR;
+        return NEG_ERROR;
     }
-    return METHOD_COUNT;
+    return NEG_METHOD_COUNT;
 }
 
 negState parseMethodCount(negParser* p, uint8_t c) {
     p->pendingMethods = c;
     if (c == 0) {
         printf("[INF] Client did not specify auth methods \n");
-        return END;
+        return NEG_END;
     }
     printf("[INF] Client specified auth methods: ");
-    return METHODS;
+    return NEG_METHODS;
 }
 
 negState parseMethods(negParser* p, uint8_t c) {
     p->pendingMethods -= 1;
     printf("%x%s", c, p->pendingMethods == 0 ? "\n" : ", ");
-    if (c == NEGOTIATION_METHOD_NO_AUTH) {
-        p->authMethod = NEGOTIATION_METHOD_NO_AUTH;
-    } else if (c == NEGOTIATION_METHOD_PASS) {
+    if (c == NEG_METHOD_NO_AUTH) {
+        p->authMethod = NEG_METHOD_NO_AUTH;
+    } else if (c == NEG_METHOD_PASS) {
         // wait until pass/user auth method is developed
     }
-    return p->pendingMethods == 0 ? END : METHODS;
+    return p->pendingMethods == 0 ? NEG_END : NEG_METHODS;
 }
 
 /*Should not happen*/
 negState parseEnd(negParser* p, uint8_t c) {
-    printf("[BUG] Trying to call negotiation parser in END state ");
-    return END;
-}
-
-/*Should not happen*/
-negState parseError(negParser* p, uint8_t c) {
-    printf("[BUG] Trying to call negotiation parser in ERROR state ");
-    return ERROR;
+    printf("[BUG] Trying to call negotiation parser in END/ERROR state ");
+    return p->state;
 }
--- src/negotiation.h
+++ src/negotiation.h
@@ -3,16 +3,16 @@
 
 #include <stdint.h>
 
-#define NEGOTIATION_METHOD_NO_AUTH 0x00
-#define NEGOTIATION_METHOD_PASS 0x02
-#define NEGOTIATION_METHOD_NO_MATCH 0xFF
+#define NEG_METHOD_NO_AUTH 0x00
+#define NEG_METHOD_PASS 0x02
+#define NEG_METHOD_NO_MATCH 0xFF
 
 typedef enum negState {
-    VERSION = 0,  // The parser is waiting for the client version
-    METHOD_COUNT, // The parser is waiting for the number of methos
-    METHODS,      // The parser is reading methods
-    END,          // All read for this state.
-    ERROR
+    NEG_VERSION = 0,  // The parser is waiting for the client version
+    NEG_METHOD_COUNT, // The parser is waiting for the number of methos
+    NEG_METHODS,      // The parser is reading methods
+    NEG_END,          // All read for this state.
+    NEG_ERROR
 } negState;
 
 typedef struct negParser {
