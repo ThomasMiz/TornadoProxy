334c9fc3021aea46a7cac02e8e81b09d99194b86
khcatino <kcatino@itba.edu.ar>
2022-11-02 22:38:13 -0300

    Begin adding state machine behaviour

--- src/socks5.c
+++ src/socks5.c
@@ -13,14 +13,119 @@
 
 #include "selector.h"
 #include "socks5.h"
+#include "stm.h"
 
 #define CLIENT_RECV_BUFFER_SIZE 4096
+
+#define N(x) (sizeof(x)/sizeof((x)[0]))
 
 typedef struct {
     TFdHandler handler;
     char* buffer;
     unsigned int bufferLength;
+    struct state_machine stm;
 } TClientData;
+
+// obtiene el struct socks5* desde la key
+#define ATTACHMENT(key) ( (TClientData *)(key)->data )
+
+enum socks_state {
+    /*
+        recibe el mensaje `hello` del cliente y lo procesa
+
+    Intereses: 
+        - OP_READ sobre client_fd
+
+    Transiciones: 
+        - HELLO_READ mientras el mensaje no esta completo
+        - HELLO_WRITE cuando esta completo
+        - ERROR ante cualquier error (IO/parseo)
+    */
+   HELLO_READ,
+
+    /*
+        envia la respuesta del `hello` al cliente
+
+    Intereses: 
+        - OP_WRITE sobre client_fd
+
+    Transiciones: 
+        - HELLO_WRITE mientras queden bytes por enviar
+        - REQUEST_READ cuando se enviaron todos los bytes
+        - ERROR ante cualquier error (IO/parseo)
+    */
+   HELLO_WRITE,
+
+    /*
+        recibe el mensaje `request` del cliente e inicia su proceso
+
+    Intereses: 
+        - OP_READ sobre client_fd
+
+    Transiciones: 
+        - REQUEST_READ mientras el mensaje no este completo
+        - REQUEST_RESOLV si quiere resolver un nombre DNS
+        - REQUEST_CONNECTING si no requiere resolver DNS y podemos inicial la conexion con el OS
+        - REQUEST_WRITE si determinamos que el mensaje no lo podemos procesar (ej. no se soporta un comando)
+        - ERROR ante cualquier error (IO/parseo)
+    */
+   REQUEST_READ,
+
+    /*
+        Espera la resolucion DNS
+
+    Intereses: 
+        - OP_NOOP sobre client_fd. Espera un evento de que la tarea bloqueante terminó
+
+    Transiciones: 
+        - REQUEST_CONNECTING si se logra la resolucion y se puede iniciar la conexion al OS.
+        - REQUEST_WRITE en otro caso
+    */
+   REQUEST_RESOLV,
+
+    /*
+        Espera que se establezca la conesion al OS
+
+    Intereses: 
+        - OP_WRITE sobre client_fd
+
+    Transiciones: 
+        - REQUEST_CWRITE cuando se haya logrado o no establecer la conexion
+    */
+   REQUEST_CONNECTING,
+
+    /*
+        Envia la respuesta del `request` al cliente
+
+    Intereses: 
+        - OP_WRITE sobre client_fd
+        - OP_NOOP sobre origin_fd
+
+    Transiciones: 
+        - HELLO_WRITE mientras queden bytes por enviar
+        - COPY si el request fue exitoso y teemos que copiar el contenido de los descriptores
+        - ERRO ante I/O error
+    */
+   REQUEST_WRITE,
+
+    /*
+        Copia bytes entre client_fd y origin_fd
+
+    Intereses: 
+        - OP_READ si hay espacio para escribir en el buffer de lectura
+        - OP_WRITE si hay bytes para leer en el buffer de escritura
+
+    Transiciones: 
+        - DONE cuando no queda nada mas por copiar
+    */
+   COPY,
+
+    // estados terminales
+    DONE,
+    ERROR,
+
+};
+
 
 void socksv5_passive_accept(TSelectorKey* key) {
     printf("New client received\n");
@@ -38,9 +143,14 @@ void socksv5_passive_accept(TSelectorKey* key) {
     }
 
     TFdHandler* handler = &clientData->handler;
-    handler->handle_read = socksv5_handle_read;
-    handler->handle_write = socksv5_handle_write;
-    handler->handle_close = socksv5_handle_close;
+    handler->handle_read = socksv5_read;
+    handler->handle_write = socksv5_write;
+    handler->handle_close = socksv5_close;
+
+    clientData->stm.initial = COPY; // TODO CAMBIAR LUEGO
+    clientData->stm.max_state = ERROR;
+    clientData->stm.states = client_statb1;
+    stm_init(&clientData->stm);
     
     TSelectorStatus status = selector_register(key->s, newClientSocket, handler, OP_READ, clientData);
     if (status != SELECTOR_SUCCESS) {
@@ -104,7 +214,7 @@ void socksv5_handle_write(TSelectorKey* key) {
     selector_set_interest_key(key, newInterests);
 }
 
-void socksv5_handle_close(TSelectorKey* key) {
+void socksv5_close(TSelectorKey* key) {
     TClientData* clientData = key->data;
 
     // Free the memory associated with this client.
@@ -116,3 +226,59 @@ void socksv5_handle_close(TSelectorKey* key) {
 
     printf("Client closed: %d\n", key->fd);
 }
+
+static void socksv5_read(TSelectorKey *key) {
+    struct state_machine *stm = &ATTACHMENT(key)->stm;
+    const enum socks_state st = stm_handler_read(stm, key);
+    // ERROR HANDLING
+}
+
+static void socksv5_write(TSelectorKey *key) {
+    struct state_machine *stm = &ATTACHMENT(key)->stm;
+    const enum socks_state st = stm_handler_write(stm, key);
+    // ERROR HANDLING
+}
+
+// definicion de handlers para cada estado
+static const struct state_definition client_statb1[] = {
+    // {
+    //     .state = HELLO_READ,
+    //     .on_arrival = hello_read_init,
+    //     .on_departure = hello_read_close,
+    //     .on_read_ready = hello_read,
+    // },
+    // {
+    //     .state = HELLO_WRITE,
+    //     .on_write_ready = hello_write,
+    // },
+    // {
+    //     .state = REQUEST_READ,
+    //     .on_arrival = request_read_init,
+    //     .on_departure = request_read_close,
+    //     .on_read_ready = request_read,
+    // },
+    // {
+    //     .state = REQUEST_RESOLV,
+    //     .on_block_ready = request_resolv_done,
+    // },
+    // {
+    //     .state = REQUEST_CONNECTING,
+    //     .on_arrival = request_connecting_init,
+    //     .on_write_ready = request_connecting,
+    // },
+    // {
+    //     .state = REQUEST_WRITE,
+    //     .on_write_ready = request_write,
+    // },
+    {
+        .state = COPY,
+        .on_read_ready = socksv5_handle_read,
+        .on_write_ready = socksv5_handle_write,
+    }, 
+    {
+        .state = DONE,
+    },
+    {
+        .state = ERROR,
+    }
+};
