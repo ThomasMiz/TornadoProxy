5334f5b16ef77665f4b41bf2aae3fea3001c4404
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2022-11-09 19:00:03 -0300

    Second take on users.h interface

--- src/users.h
+++ src/users.h
@@ -1,46 +1,88 @@
 #ifndef _USERS_H_
 #define _USERS_H_
 
-#define MAX_USERNAME_LENGTH 31
-#define MAX_PASSWORD_LENGTH 31
+/** The maximum length of a user's username. */
+#define USERS_MAX_USERNAME_LENGTH 31
+/** The maximum length of a user's password. */
+#define USERS_MAX_PASSWORD_LENGTH 31
 
-#define DEFAULT_USERS_FILE "users.txt"
+/** The file on disk to which user data is saved. */
+#define USERS_DEFAULT_FILE "users.txt"
+
+/** The maximum amount of users the system supports. */
+#define USERS_MAX_COUNT 100
+
+/** When no users are present, a default admin user is created with this name and password */
+#define USERS_DEFAULT_USERNAME "admin"
+#define USERS_DEFAULT_PASSWORD "admin"
 
 #include "selector.h"
 
 /**
+ * Represents a user's privilige level.
+*/
+typedef enum {
+    UPRIV_USER = 0,
+    UPRIV_ADMIN = 1
+} TUserPriviligeLevel;
+
+/**
+ * Defines the possible status codes returned by functions from the users module.
+*/
+typedef enum {
+    EUSER_OK = 0,
+    EUSER_WRONGUSERNAME = 1,
+    EUSR_ALREADYEXISTS = 2,
+    EUSR_WRONGPASSWORD = 3,
+    EUSR_LIMITREACHED = 4,
+    EUSR_CREDTOOLONG = 5,
+    EUSR_BADOPERATION = 6,
+    EUSR_UNKNOWNERROR = -1,
+} TUserStatus;
+
+/**
  * @brief Initializes the users system.
- * @param selector The selector to use. This is requried in order to write to the
- * users file with non-blocking calls.
  * @param usersFile The file in disk in which to load and store user data. Set to
  * NULL or an empty string to use the default users file.
 */
-int usersInit(TSelector selector, const char* usersFile);
+int usersInit(const char* usersFile);
 
 /**
  * @brief Checks whether a given username exists and verifies that it's password
  * matches.
  * @param username The username of the user to check.
  * @param password The password of the user to check.
- * @returns 0 if the user exists and the password matches. 1 if the user exists,
- * but the password doesn't match, or 2 if the username doesn't exist.
+ * @param outLevel A pointer to a variable where the user's privilige level will be written to.
+ * @returns A value from TUserStatus. Either OK, WRONGPASSWORD, WRONGUSERNAME,
+ * UNKNOWNERROR.
 */
-int usersLogin(const char* username, const char* password);
+TUserStatus usersLogin(const char* username, const char* password, TUserPriviligeLevel* outLevel);
 
 /**
- * @brief Creates a user with the given username and password.
- * @param username The username to give the new user.
- * @param password The password to give the new user.
+ * @brief Creates a user with the given username and password, or updates an
+ * existing user's password or privilige level.
+ * @param username The username for the user.
+ * @param password The password for the user.
  * @param updatePassword Whether to update the password if the user already exists.
- * @returns 0 if the user was created successfully, 1 if the user already exists,
- * or -1 for other unspecified errors.
+ * @param privilige The privilige level for the user.
+ * @param updatePrivilige Whether to update the user's privilige level if the user
+ * already exists.
+ * @returns A value from TUserStatus. Either OK, ALREADYEXISTS, UNKNOWNERROR,
+ * LIMITREACHED, or CREDTOOLONG.
 */
-int usersCreate(const char* username, const char* password, int updatePassword);
+TUserStatus usersCreate(const char* username, const char* password, int updatePassword, TUserPriviligeLevel privilige, int updatePrivilige);
 
 /**
- * @brief Shuts down the user system, ensuring any remaining data on memory is
- * flushed to disk.
+ * @brief Deletes a user from the system.
+ * @param username The username of the user to delete.
+ * @returns A value from TUserStatus. Either OK, WRONGUSERNAME, BADOPERATION (if the
+ * user is the last administrator on the system), or UNKNOWNERROR.
 */
-int usersFinalize();
+TUserStatus usersDelete(const char* username);
+
+/**
+ * @brief Shuts down the user system, flushing the users to a file on disk.
+*/
+TUserStatus usersFinalize();
 
 #endif
